1d7735c3043219470e8b4c9cf87cd42ca0ac75f6
Gustavo <ravelsoares.lima@gmail.com>
2021-08-16 03:14:46 +0100

    Configured postgres db.

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                       PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -13,7 +14,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
